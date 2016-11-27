5bcadf7bb1382fda989ed0b148c2f4252295aee9
James Collings <james@jclabs.co.uk>
2016-11-27 20:38:03 +0000

    Add comments and convert to wpcs

--- wpdf.php
+++ wpdf.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Plugin Name: WP Form Builder
  * Plugin URI: https://www.wpformbuilder.com/
@@ -14,44 +13,89 @@
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
+	exit; // Exit if accessed directly.
 }
 
+/**
+ * Class WPDF_DeveloperForms
+ */
 class WPDF_DeveloperForms {
 
+	/**
+	 * Plugin Version
+	 *
+	 * @var string
+	 */
 	protected $version = '0.3';
+
+	/**
+	 * Plugin Directory
+	 *
+	 * @var bool|string
+	 */
 	public $plugin_dir = false;
+
+	/**
+	 * Plugin Url
+	 *
+	 * @var bool|string
+	 */
 	public $plugin_url = false;
+
+	/**
+	 * Plugin slug
+	 *
+	 * @var bool|string
+	 */
 	protected $plugin_slug = false;
+
+	/**
+	 * Plugin Settings array
+	 *
+	 * @var bool
+	 */
 	protected $settings = false;
 
 	/**
 	 * List of all registered forms
+	 *
 	 * @var WPDF_Form[]
 	 */
 	protected $_forms = null;
 
 	/**
 	 * Active Form
+	 *
 	 * @var WPDF_Form
 	 */
 	protected $_form = null;
 
 	/**
+	 * Default plugin settings
+	 *
 	 * @var array
 	 */
 	private $default_settings = null;
 
 	/**
+	 * Plugin text
+	 *
 	 * @var WPDF_Text
 	 */
 	public $text = null;
 
 	/**
 	 * Single instance of class
+	 *
+	 * @var null
 	 */
 	protected static $_instance = null;
 
+	/**
+	 * Return current instance of class
+	 *
+	 * @return WPDF_DeveloperForms
+	 */
 	public static function instance() {
 		if ( is_null( self::$_instance ) ) {
 			self::$_instance = new self();
@@ -59,25 +103,29 @@ class WPDF_DeveloperForms {
 		return self::$_instance;
 	}
 
-	public function __construct(){
+	/**
+	 * WPDF_DeveloperForms constructor.
+	 */
+	public function __construct() {
 
-		$this->plugin_dir =  plugin_dir_path( __FILE__ );
+		$this->plugin_dir = plugin_dir_path( __FILE__ );
 		$this->plugin_url = plugins_url( '/', __FILE__ );
-		$this->plugin_slug = basename(dirname(__FILE__));
+		$this->plugin_slug = basename( dirname( __FILE__ ) );
 
-		// not in init so function can be called from theme functions file
 		$this->init();
 
 		add_action( 'wp_loaded', array( $this, 'load_db_forms' ), 9 );
 		add_action( 'wp_loaded', array( $this, 'process_form' ) );
-		//add_action( 'init', array( $this, 'init' ) );
 
-		register_activation_hook( __FILE__, array($this, 'on_activation') );
+		register_activation_hook( __FILE__, array( $this, 'on_activation' ) );
 
-		add_action('wp_enqueue_scripts', array($this, 'enqueue_scripts'));
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
 	}
 
-	public function includes(){
+	/**
+	 * Load all necessary files
+	 */
+	public function includes() {
 
 		include_once 'libs/class-wpdf-form.php';
 		include_once 'libs/class-wpdf-db-form.php';
@@ -101,7 +149,6 @@ class WPDF_DeveloperForms {
 		include_once 'libs/wpdf-functions.php';
 		include_once 'libs/wpdf-shortcodes.php';
 
-		// modules
 		include_once 'libs/modules/class-wpdf-user-registration.php';
 
 		if ( is_admin() ) {
@@ -111,184 +158,244 @@ class WPDF_DeveloperForms {
 	}
 
 	/**
-	 * Initite plugin
+	 * Initiate plugin
+	 *
 	 * @return void
 	 */
-	public function init(){
+	public function init() {
 
 		$this->load_settings();
 		$this->includes();
 
-		// load text
+		// load text.
 		$this->text = new WPDF_Text();
 
-		// load preview
-		if(isset($_GET['wpdf_preview']) && !empty($_GET['wpdf_preview'])){
+		// load preview.
+		$preview = isset( $_GET['wpdf_preview'] ) && !empty( $_GET['wpdf_preview'] ) ? esc_attr($_GET['wpdf_preview']) : false;
+		if ( $preview ) {
 			include_once 'libs/class-wpdf-preview.php';
-			new WPDF_Preview(sanitize_title($_GET['wpdf_preview']));
+			new WPDF_Preview( $preview );
 		}
 	}
 
 	/**
 	 * Plugin Default Settings
+	 *
 	 * @return void
 	 */
-	public function default_settings(){
+	public function default_settings() {
 
-		// set default settings
-		$this->default_settings = array(
-
-		);
+		$this->default_settings = array();
 	}
 
 	/**
 	 * Load plugin settings
+	 *
 	 * @return void
 	 */
-	public function load_settings(){
+	public function load_settings() {
 
 		$this->default_settings();
 		$this->settings = array();
 
-		// load settings from db
-		foreach($this->default_settings as $key => $default){
-			$data = get_option('wpdf_'.$key, $default);
-			$this->settings[$key] = is_serialized( $data ) ? unserialize( $data ) : $data;
+		// load settings from db.
+		foreach ( $this->default_settings as $key => $default ) {
+			$data = get_option( 'wpdf_' . $key, $default );
+			$this->settings[ $key ] = is_serialized( $data ) ? unserialize( $data ) : $data;
 		}
 	}
 
-	public function get_settings($key, $default = false){
+	/**
+	 * Get plugin setting
+	 *
+	 * @param string $key setting key.
+	 * @param bool   $default load default value.
+	 *
+	 * @return bool|mixed
+	 */
+	public function get_settings( $key, $default = false ) {
 
-		if($default){
-			return isset($this->default_settings[$key]) ? $this->default_settings[$key] : false;
+		if ( $default ) {
+			return isset( $this->default_settings[ $key ] ) ? $this->default_settings[ $key ] : false;
 		}
 
-		return isset($this->settings[$key]) ? $this->settings[$key] : false;
+		return isset( $this->settings[ $key ] ) ? $this->settings[ $key ] : false;
 	}
 
-	public function get_version(){
+	/**
+	 * Get plugin version
+	 *
+	 * @return string
+	 */
+	public function get_version() {
 		return $this->version;
 	}
-	public function get_plugin_slug(){
+
+	/**
+	 * Get plugin slug
+	 *
+	 * @return bool|string
+	 */
+	public function get_plugin_slug() {
 		return $this->plugin_slug;
 	}
-	public function get_plugin_url(){
+
+	/**
+	 * Get plugin Url
+	 *
+	 * @return bool|string
+	 */
+	public function get_plugin_url() {
 		return $this->plugin_url;
 	}
-	public function get_plugin_dir(){
+
+	/**
+	 * Get plugin path to directory
+	 *
+	 * @return bool|string
+	 */
+	public function get_plugin_dir() {
 		return $this->plugin_dir;
 	}
 
-	public function enqueue_scripts(){
+	/**
+	 * Load plugin scripts and styles
+	 */
+	public function enqueue_scripts() {
 
 		$ext = '.min';
 		$version = $this->get_version();
-		if((defined('WP_DEBUG') && WP_DEBUG) || (defined('SCRIPT_DEBUG') && SCRIPT_DEBUG)){
+		if ( ( defined( 'WP_DEBUG' ) && WP_DEBUG) || ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ) {
 			$version = time();
 			$ext = '';
 		}
 
-		// todo: check if viewing form and that form has recaptcha enabled, then output scripts in head
-		wp_enqueue_script('wpdf-recaptcha', '//www.google.com/recaptcha/api.js');
+		// todo: check if viewing form and that form has recaptcha enabled, then output scripts in head.
+		wp_enqueue_script( 'wpdf-recaptcha' , '//www.google.com/recaptcha/api.js' );
 
-		wp_enqueue_script('wpdf-main', $this->get_plugin_url() . 'assets/public/js/main'.$ext.'.js', array('jquery'), $version, true);
-		wp_enqueue_style('wpdf-main', $this->get_plugin_url() . 'assets/public/css/main'.$ext.'.css', array(), $version);
+		wp_enqueue_script( 'wpdf-main' , $this->get_plugin_url() . 'assets/public/js/main' . $ext . '.js', array( 'jquery' ), $version, true );
+		wp_enqueue_style( 'wpdf-main' , $this->get_plugin_url() . 'assets/public/css/main' . $ext . '.css', array(), $version );
 	}
 
-	public function on_activation(){
-//		add_option( 'Activated_Plugin_WPDF', 'WPDF' );
+	/**
+	 * On Plugin Activation
+	 */
+	public function on_activation() {
 
 		$db = new WPDF_DatabaseManager();
-		if($db->install()){
-			//delete_option( 'Activated_Plugin_WPDF' );
-		}
+		$db->install();
 	}
 
-	#region Register Form Settings
+	/**
+	 * Register new form
+	 *
+	 * @param string $name Name of form.
+	 * @param array  $args Form arguments.
+	 *
+	 * @return WPDF_Form
+	 */
+	public function register_form( $name, $args = array() ) {
 
-	public function register_form($name, $args = array()){
-
-		// register form with system
-		$form = new WPDF_Form($name, $args);
-		$this->_forms[$name] = $form;
+		// register form with system.
+		$form = new WPDF_Form( $name, $args );
+		$this->_forms[ $name ] = $form;
 		return $form;
 	}
 
 	/**
 	 * Load form, and initialise it if required
-	 * @param $name
+	 *
+	 * @param string $name Name of plugin.
 	 *
 	 * @return bool|WPDF_Form
 	 */
-	public function get_form($name){
+	public function get_form( $name ) {
 
-		if(isset($this->_forms[$name])){
+		if ( isset( $this->_forms[ $name ] ) ) {
 
-			if(is_array($this->_forms[$name])){
+			if ( is_array( $this->_forms[ $name ] ) ) {
 
-				if(isset($this->_forms[$name]['form_id'])){
-					// load db form
-					$this->_forms[$name] = new WPDF_DB_Form($this->_forms[$name]['form_id']);
+				if ( isset( $this->_forms[ $name ]['form_id'] ) ) {
+					// load db form.
+					$this->_forms[ $name ] = new WPDF_DB_Form( $this->_forms[ $name ]['form_id'] );
 				}
 			}
 
-			$form = $this->_forms[$name];
-
+			$form = $this->_forms[ $name ];
 			return $form;
 		}
 
 		return false;
 	}
 
-	public function process_form(){
+	/**
+	 * Check and process form submission requests
+	 */
+	public function process_form() {
 
-		// load and process current form
-		if( $this->get_current_form() ){
+		// load and process current form.
+		if ( $this->get_current_form() ) {
 			$this->_form->process();
 		}
 	}
 
-	public function get_current_form(){
+	/**
+	 * Get currently loaded form
+	 *
+	 * @return bool|WPDF_Form
+	 */
+	public function get_current_form() {
 
-		if($this->_form != null){
+		if ( null !== $this->_form ) {
 			return $this->_form;
 		}
 
 		// find active form, maybe a hidden form action field was submitted?
-		// if so load that form
-		if(isset($_POST['wpdf_action'])){
+		// if so load that form.
+		$form_id = isset($_POST['wpdf_action']) ? esc_attr($_POST['wpdf_action']) : false;
+		if ( $form_id ) {
 
-			$form_id = $_POST['wpdf_action'];
-			$this->_form = $this->get_form($form_id);
+			$this->_form = $this->get_form( $form_id );
 			return $this->_form;
 		}
 
 		return false;
 	}
 
-	#endregion
-
-	public function get_forms(){
+	/**
+	 * Get list of forms
+	 *
+	 * @return WPDF_Form[]
+	 */
+	public function get_forms() {
 		return $this->_forms;
 	}
 
-	public function load_db_forms(){
+	/**
+	 * Get forms from database
+	 */
+	public function load_db_forms() {
 
 		$query = new WP_Query(array(
 			'post_type' => 'wpdf_form',
 			'posts_per_page' => -1,
-			'fields' => 'ids'
+			'fields' => 'ids',
 		));
 
-		if($query->have_posts()){
-			foreach($query->posts as $id){
-				$this->_forms['WPDF_FORM_'.$id] = array('form_id' => $id); //new WPDF_DB_Form($id);
+		if ( $query->have_posts() ) {
+			foreach ( $query->posts as $id ) {
+				$form_key = sprintf( 'WPDF_FORM_%d', $id );
+				$this->_forms[ $form_key ] = array( 'form_id' => $id );
 			}
 		}
-
 	}
 }
 
+/**
+ * Globally access WPDF_DeveloperForms instance.
+ *
+ * @return WPDF_DeveloperForms
+ */
 function WPDF() {
 	return WPDF_DeveloperForms::instance();
 }
